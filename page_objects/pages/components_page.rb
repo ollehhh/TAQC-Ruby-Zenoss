346b9a7bf29f7e3f617d916bfbfbf5d949e229ac
Dima <phaqoff@gmail.com>
2021-07-15 22:48:10 +0300

    components_page

--- page_objects/pages/components_page.rb
+++ page_objects/pages/components_page.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class ComponentsPage < SitePrism::Page
-  set_url(ENV['COMPONENTS_PAGE_URL'])
   # section :main_bar, MainBarSection, '#devdetailbar'
-  section :interfaces, ToolBar, '#ext-gen1656'
+  section :interfaces, ToolBar, '#component_card-body :first-child .x-toolbar'
   section :components_table, ComponentsTable, '#component_card div:nth-of-type(1) div'
 end
