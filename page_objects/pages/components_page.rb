9bc439a64a8786f22d897ac2b376eca4e308e21e
Dima <phaqoff@gmail.com>
2021-07-16 15:59:13 +0300

    components_page

--- page_objects/pages/components_page.rb
+++ page_objects/pages/components_page.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class ComponentsPage < SitePrism::Page
-
+  section :left_bar_section, ComponentleftbarSection, '#detailContainer'
   section :main_bar, MainBarSection, '#devdetailbar'
   section :interfaces, ToolBar, '#component_card-body :first-child .x-toolbar'
-  section :components_table, ComponentsTable, '#component_card div:nth-of-type(1) div.x-toolbar'
+  section :components_table, ComponentsTable, '#component_card div:nth-of-type(1) div'
 end
+
