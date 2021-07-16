728c96e2440e23f670a4e45d90d848425454d1be
Dima <phaqoff@gmail.com>
2021-07-16 16:07:35 +0300

    components_page

--- page_objects/pages/components_page.rb
+++ page_objects/pages/components_page.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class ComponentsPage < SitePrism::Page
-  section :left_bar_section, ComponentleftbarSection, '#detailContainer'
   section :main_bar, MainBarSection, '#devdetailbar'
+  section :left_bar_section, ComponentleftbarSection, '#detailContainer'
   section :interfaces, ToolBar, '#component_card-body :first-child .x-toolbar'
   section :components_table, ComponentsTable, '#component_card div:nth-of-type(1) div'
 end
