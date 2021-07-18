f62bf986977421679c4d415d997ae653631a2526
andrey.radushev <radushev123@gmail.com>
2021-07-18 16:09:36 +0300

    modif: add def functional click on graphs

--- features/support/click_on_component_helper.rb
+++ features/support/click_on_component_helper.rb
@@ -3,6 +3,11 @@ module ClickOnComponentHelper
     @overview_device_page = OverviewDevicePage.new
     @overview_device_page.left_bar_section.find(:xpath, ".//td[contains(.,'#{component}')]").click
   end
+
+  def click_on_graphs
+    @overview_device_page = OverviewDevicePage.new
+    @overview_device_page.left_bar_section.component_tr[4].td_element.click
+  end
 end
 
 World(ClickOnComponentHelper)
