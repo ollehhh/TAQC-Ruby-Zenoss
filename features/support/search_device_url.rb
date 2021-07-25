d56e652911e7b29a4a1fa8c14eb36ebacb90a639
andrey.radushev <radushev123@gmail.com>
2021-07-25 17:37:47 +0300

    fix helper methods to rubocop&reek

--- features/support/search_device_url.rb
+++ features/support/search_device_url.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
+# This module is responsible for finding a device by URL
 module SearchByURLHelper
   def search_url(url)
     @inf_page = InfPage.new
-    @inf_page.devise_table.find("a[href='#{url}']").click()
+    @inf_page.devise_table.find("a[href='#{url}']").click
   end
 end
 
